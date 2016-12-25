e7d055cd756de638922bd251b273cb510835e494
Sobchenyuk <ablehands@ukr.net>
2016-12-25 22:09:50 +0200

    8_lessons

---
+++ 8_lessons/002_Forms/Validate.js
@@ -0,0 +1,65 @@
+﻿// регистрация события загрузки документа.
+if (window.addEventListener) window.addEventListener("load", init, false);
+else if (window.attachEvent) window.attachEvent("onload", init);
+
+// регистрация обработчиков событий элементов формы.
+function init() {
+    form1.userName.onchange = nameOnChange;
+    form1.email.onchange = emailOnChange;
+    form1.zip.onchange = zipcodeOnChange;
+    form1.onsubmit = onsubmitHandler;
+}
+
+// метод проверки значения в элементе по регулярному выражению.
+function validate(elem, pattern) {
+    var res = pattern.test(elem.value);
+    if (res === false) {
+        elem.className = "invalid";
+    } // установка CSS класса 
+    else {
+        elem.className = "valid";
+    }
+}
+
+// обработчики событий изменения текста в окне.
+function nameOnChange() {
+    var pattern = /\S/;
+    validate(this, pattern);
+}
+
+function emailOnChange() {
+    var pattern = /\b[a-z0-9._]+@[a-z0-9.-]+\.[a-z]{2,4}\b/i;
+    validate(this, pattern);
+}
+
+function zipcodeOnChange() {
+    var pattern = /\d{5}/;
+    validate(this, pattern);
+}
+
+// событие при отправке формы на сервер.
+function onsubmitHandler(e) {
+
+    for (var i = 0; i < form1.elements.length; ++i) {
+        if (form1.elements[i].type === "text")
+            form1.elements[i].className = "valid";
+    }
+
+    var invalid = false;
+
+    for (var i = 0; i < form1.elements.length; ++i) {
+        var elem = form1.elements[i];
+        // проверка типа элемента и наличия обработчика события onchange.
+        if (elem.type == "text" && elem.onchange) {
+            elem.onchange(); // запуск события onchanhe
+            if (elem.className == "invalid") invalid = true;
+        }
+    }
+
+    if (invalid) {
+        alert("Допущены ошибки при заполнении формы.");
+        e.preventDefault();
+        return false; // отмена отправки формы.
+    }
+}
+
